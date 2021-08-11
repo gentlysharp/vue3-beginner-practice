628e0063282f87e2c061a023ebb71b7d3d11adc5
bendotman <ben.han@dotman.co.kr>
2021-08-11 13:17:48 +0900

    not found 페이지 처리

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import NotFound from '../views/NotFound.vue'
 // import About from '../views/About.vue'
 
 const routes = [
@@ -28,6 +29,17 @@ const routes = [
     component: () => import(/* webpackChunkName: "job-details" */ '../views/jobs/JobDetails.vue'),
     props: true
   },
+  // redirect
+  {
+    path:'/all-jobs',
+    redirect:'/jobs'
+  },
+  // carchall 404
+  // :catchAll(.*)는 마치 함수와 같은 패턴으로 위에 정의된 모든 패턴 이외 다른패턴을 요청하게 되면 404 페이지를 보여준다.
+  {
+    path: '/:catchAll(.*)',
+    component: NotFound
+  }
 
 ]
 
